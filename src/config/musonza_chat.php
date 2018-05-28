b03c70419724c5b5609af585f50881164c41920b
Tinashe Musonza <tinashemusonza@gmail.com>
2018-05-28 13:59:47 -0700

    Apply fixes from StyleCI (#85)

--- src/config/musonza_chat.php
+++ src/config/musonza_chat.php
@@ -3,11 +3,11 @@
 return [
     'user_model'            => 'App\User',
 
-    /**
+    /*
      * This will allow you to broadcast an event when a message is sent
      * Example:
-     * Channel: private-mc-chat-conversation.2, 
-     * Event: Musonza\Chat\Messages\MessageWasSent 
+     * Channel: private-mc-chat-conversation.2,
+     * Event: Musonza\Chat\Messages\MessageWasSent
      */
     'broadcasts'            => false,
 
