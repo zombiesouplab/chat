039fd152c38e8f61e83a8fc934575cc777f91357
Tinashe Musonza <tinashemusonza@gmail.com>
2019-10-27 11:14:09 -0400

    Apply fixes from StyleCI (#182)

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -9,7 +9,6 @@ use Musonza\Chat\Models\Message;
 use Musonza\Chat\Tests\Helpers\Models\Bot;
 use Musonza\Chat\Tests\Helpers\Models\Client;
 use Musonza\Chat\Tests\Helpers\Models\User;
-use DB;
 
 class MessageTest extends TestCase
 {
