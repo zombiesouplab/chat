88909a7d7b87aa8753080ea992e28b8594112ba8
Tinashe Musonza <tinashemusonza@gmail.com>
2019-10-27 00:09:58 -0400

    Travis config (#177)
    * wip

--- tests/Helpers/factories/ModelFactory.php
+++ tests/Helpers/factories/ModelFactory.php
@@ -23,7 +23,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name'           => $faker->name,
         'email'          => $faker->unique()->safeEmail,
         'password'       => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'remember_token' => 'xahja87ahjahajhajhja',
     ];
 });
 
